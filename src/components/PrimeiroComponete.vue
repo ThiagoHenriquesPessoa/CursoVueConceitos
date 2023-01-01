f38bfc13df2328fa74614c7adef37995f22462b1
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-01 12:01:48 -0300

    inserino dados atravez de componentes

--- src/components/PrimeiroComponete.vue
+++ src/components/PrimeiroComponete.vue
@@ -1,10 +1,18 @@
 <template>
 <h1>Olá Mundo!</h1>
+<p>Meu nome é {{ name }} e trabalho como {{ profissao }}</p>
 </template>
 
 <script>
 
 export default {
-    name: 'PrimeiroComponete'
+    name: 'PrimeiroComponete',
+    data(){
+        return{
+            name: "Thiago",
+            profissao: "Dev C# junior"
+        }
+    }
+    
 }
 </script>
