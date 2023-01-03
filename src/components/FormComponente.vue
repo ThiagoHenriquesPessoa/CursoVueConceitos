be70aabaf702fed1cb9e69165d62983bd8d05737
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-02 23:40:13 -0300

    Hierarquia e forms

---
+++ src/components/FormComponente.vue
@@ -0,0 +1,20 @@
+<template>
+    <form action="">
+        <div><InputTextForm/></div>
+        <div> <InputTextForm/></div>
+       <div><SubmitForm/></div>
+        
+    </form>
+</template>
+
+<script>
+import InputTextForm from './form/InputTextForm.vue';
+import SubmitForm from './form/SubmitForm.vue';
+export default{
+    name: 'FormComponente',
+    components:{
+        InputTextForm,
+        SubmitForm
+    }
+}
+</script>
--- src/components/PessoaComponente.vue
+++ src/components/PessoaComponente.vue
@@ -1,14 +1,17 @@
 <template>
     <h2>Essa é a descrição de {{ nome }}</h2>
     <InfoComponente/>
+    <FormComponente/>
 </template>
 
 <script>
-import InfoComponente from './InfoComponente'
+import InfoComponente from './InfoComponente.vue'
+import FormComponente from './FormComponente.vue';
 export default{
     name: 'PessoaComponente',
     components:{
-        InfoComponente
+        InfoComponente,
+        FormComponente
     },
     data(){
         return{
