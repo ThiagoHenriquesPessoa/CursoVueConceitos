ec2e29e5ca70a2ef011755916d5a9ca11db0a691
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-03 22:27:19 -0300

    trabalhando com diretivas

--- src/components/InfoComponente.vue
+++ src/components/InfoComponente.vue
@@ -1,5 +1,6 @@
 <template>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utiliso as seguintes tecnologias</p>
     <ul>
         <li>JavaScript</li>
@@ -7,11 +8,18 @@
         <li>CSharp</li>
         <li>SqlServer</li>
     </ul>
+    <p v-show="mostrar_email">mande uma mensagem para: {{ email }}</p>
 </template>
 
 <script>
-
 export default ({
-   name: 'InfoComponente'
+   name: 'InfoComponente',
+   data(){
+    return{
+        esta_trabalhando: false,
+        mostrar_email: false,
+        email: 'thiago@gmail.com'
+    }
+   }
 })
 </script>
