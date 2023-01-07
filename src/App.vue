61e8836ded2f3c223c581e1a47b87abf463e5c42
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-07 09:36:56 -0300

    Estilizacao global e scoped

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  <HeaderComponente/>
   <PrimeiroComponete/>
   <LifeCycle/>
   <PessoaComponente/>
 </template>
 
 <script>
+import HeaderComponente from './components/HeaderComponente.vue';
 import PrimeiroComponete  from './components/PrimeiroComponete.vue'
 import LifeCycle from './components/LifeCycle.vue'
 import PessoaComponente from './components/PessoaComponente.vue';
@@ -12,9 +14,25 @@ import PessoaComponente from './components/PessoaComponente.vue';
 export default {
     name: 'App',
     components:{
+      HeaderComponente,
       PrimeiroComponete,
       LifeCycle,
       PessoaComponente
     }
 }
 </script>
+
+<style>
+body{
+  background-color: #333;
+  color: #fff;
+}
+a{
+  color: red;
+}
+
+.link{
+  background-color: black;
+  color: aqua;
+}
+</style>
---
+++ src/components/HeaderComponente.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projetos</a></li>
+        <li><a href="#">Contatos</a></li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderComponente'
+}
+</script>
+
+<style scoped>
+ul{
+    list-style: none;
+    display: flex;
+    background-color: black;
+}
+
+li{
+    margin-right: 10px;
+}
+
+a{
+    color: #ddd;
+    text-decoration: none;
+    transition: .5s;
+}
+
+a:hover{
+    color: blue;
+}
+</style>
