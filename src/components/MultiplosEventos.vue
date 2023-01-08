b9a8ae1d4867439a14dfdabfad5d5f187dfd9e32
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-08 11:04:19 -0300

    Ativando multiplos eventos

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1), segundo(txt2), terceiro()">Ativando multiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'MultiplosEventos',
+    data() {
+        return{
+        multiplos_eventos: "",
+        txt1: "Primeiro evento ativado!",
+        txt2: "Segundo evento ativado!"
+        }
+    },
+    methods:{
+        primeiro(txt){
+            this.multiplos_eventos = txt
+        },
+        segundo(txt){
+            setTimeout(() => this.multiplos_eventos = txt, 1000)
+        },
+        terceiro(){
+            setTimeout(() => this.multiplos_eventos = "", 2000)
+        }
+    }
+}
+</script>
+<style scoped>
+div{
+    margin: 100px;
+}
+</style>
